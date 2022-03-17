66fd2b8c9d926a2f089b691f584fbde0132c859b
Oculareo <oculareo@gmail.com>
2022-03-17 16:44:08 -0400

    Added remove todo function with type checking

--- Ch-14/react-01-first-component-props/src/components/TodoList.tsx
+++ Ch-14/react-01-first-component-props/src/components/TodoList.tsx
@@ -4,6 +4,7 @@ import React from "react";
 // with id: string, and text: string
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 // FC tells Typescript that this is a functional component that accepts <TodoListProps>
@@ -11,7 +12,12 @@ const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
       ))}
     </ul>
   );
